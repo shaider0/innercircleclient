883ed28b1649e2adb6397af86745171075479f77
SHaider0 <Samnoon.Haider@gmail.com>
2020-04-05 22:51:13 -0400

    fix welcome-message alignment issues

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -18,15 +18,13 @@ class WelcomeMessage extends Component {
       <button onClick={this.closeMessage} style={{"float": "right"}} className="btn btn-danger">Don't Show This Message Again</button>
       <em>
         <h4>Welcome to InnerCircle!</h4>
-        <p>To get started:</p>
-        <ol>
-          <li className="welcome-msg-li">Add a profile picture</li>
-          <li className="welcome-msg-li">Search for friends by username and send friend requests</li>
-          <li className="welcome-msg-li">Accept incoming friend requests</li>
-          <li className="welcome-msg-li">Add a new post to share what's on your mind. Include an image if you'd like!</li>
-          <li className="welcome-msg-li">Send messages to your friends</li>
-          <li className="welcome-msg-li">Have fun!</li>
-        </ol>
+        <h5 className="getstarted">To get started:</h5>
+          <p>-Add a profile picture</p>
+          <p>-Search for friends by username and send friend requests</p>
+          <p>-Accept incoming friend requests</p>
+          <p>-Add a new post to share what's on your mind. Include an image if you'd like!</p>
+          <p>-Send messages to your friends</p>
+          <p>-Have fun!</p>
       </em>
       </div>
     )
